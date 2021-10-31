a6f0b8d1173f417b52ef2603b21ae4fb5f096d76
bemzhao <bem.zhao@foxmail.com>
2021-10-31 12:23:58 +0800

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -11,17 +11,18 @@ cd docs/.vuepress/dist
 
 if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy'
-  githubUrl=git@github.com:bemzhao/bemzhao.github.io.git
+  # githubUrl=git@github.com:bemzhao/bemzhao.github.io.git
 else
   msg='来自 github actions 的自动部署'
-  githubUrl=https://bemzhao:${GITHUB_TOKEN}@github.com/bemzhao/bemzhao.github.io.git
+  # githubUrl=https://bemzhao:${GITHUB_TOKEN}@github.com/bemzhao/bemzhao.github.io.git
 fi
 git init
 git add -A
 git commit -m "${msg}"
 git config --global user.email "bem.zhao@foxmail.com"
 git config --global user.name "bemzhao"
-git push -f $githubUrl master # 推送到github master分支
+git remote add origin https://github.com/bemzhao/bemzhao.github.io.git
+git push -f origin master # 推送到github master分支
 
 cd -
 rm -rf docs/.vuepress/dist
